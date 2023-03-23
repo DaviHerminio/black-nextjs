85ed77b0c8382798dd95dc68940c9d3d13364c14
Davi <daviherminio@gmail.com>
2023-03-23 11:48:35 -0300

    primeiro commit

---
+++ pages/cart.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import { NextPage } from 'next'
+
+
+
+
+const  Cart: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Carrinho</h1>
+
+        
+      </main>
+    </>
+  )
+}
+export default Cart
---
+++ pages/products.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import { NextPage } from 'next'
+
+
+
+
+const Products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça todos os nossos produtos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Nossos Produtos</h1>
+
+        
+      </main>
+    </>
+  )
+}
+export default Products
